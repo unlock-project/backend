63a2f8761fb0272f1a18d67d30d54d1f9a1bc716
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-08-02 01:44:21 +0500

    #4 added broadcast publish services

--- bot_app/services.py
+++ bot_app/services.py
@@ -1,56 +1,67 @@
 import json
-from microservice_request.services import ConnectionService
 import requests
 from django.conf import settings
-from .models import Message, Vote, VoteOption, Registry, RegistryEvent
-from django.core import serializers
+from .models import Message, Question, Vote, VoteOption, Registry, RegistryEvent
 
 
 def broadcast_message(message: Message):
-    service = settings.BOT_URL+"/message/publish"
+    service = settings.BOT_URL + "message/publish"
     data = {
-            "message_id": message.id,
-            "message_text": message.text,
-        }
-    response = requests.post(service, data=json.dumps(data))
-    return response.json()
+        "message_id": message.id,
+        "message_text": message.text,
+    }
+    response = requests.post(service, data=data)
+    return response
+
+
+def broadcast_question(question: Question):
+    service = settings.BOT_URL + "question/publish"
+    data = {
+        "question_id": question.id,
+        "question_text": question.text,
+    }
+    json_data = json.dumps(data)
+    response = requests.post(service, data=data)
+    return response
 
 
 def publish_vote(vote: Vote):
-    service = settings.BOT_URL + "/vote/publish"
+    service = settings.BOT_URL + "vote/publish"
     options = VoteOption.objects.filter(voting__id=vote.id)
     options_serial = []
     for option in options:
         options.append({
-            "id": option.id,
-            "text": option.text,
+            "option_id": option.id,
+            "option_text": option.text,
         })
 
     data = {
-            "vote_id": vote.id,
-            "vote_text": vote.text,
-            "options": options,
-        }
-    response = requests.post(service, data=json.dumps(data))
-    return response.json()
+        "vote_id": vote.id,
+        "vote_text": vote.text,
+        "options": options,
+    }
+    response = requests.post(service, data=data)
+    return response
 
 
 def publish_registry(registry: Registry):
-    service = settings.BOT_URL + "/registry/publish"
+    service = settings.BOT_URL + "registry/publish"
     events = RegistryEvent.objects.filter(registry_id=registry.id)
     options = []
     for event in events:
-        options.append(event.bot_text)
+        options.append({
+            "option_id": event.id,
+            "option_text": event.bot_text,
+        })
 
     data = {
-        "event_id": registry.event.id if registry.event is not None else None,
-        "text": registry.text,
+        "registration_id": registry.id,
+        "registration_text": registry.text,
         "options": options,
     }
 
-    response = requests.post(service, data=json.dumps(data))
-    return response.json()
-
+    response = requests.post(service, data=data)
+    return response
 
 
 def checkinitdata(_auth: str) -> dict:
--- bot_app/urls.py
+++ bot_app/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-
 from .api import api as bot_api
-from .views import qrscanner_page, error_page, test
+from .views import qrscanner_page, error_page
 
 urlpatterns = [
     path('scanner', qrscanner_page),
     path('error/<int:error_id>/', error_page),
     path('api/', bot_api.urls),
-    path('test/', test),
 ]
