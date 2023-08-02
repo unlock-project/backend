415673f320b2c61da2d33e869b85c439fc196bb3
MulhamShaheen <mulhamshaheen.ms@gmail.com>
2023-08-02 12:05:24 +0500

    #9 user's team endpoint

--- users_app/api.py
+++ users_app/api.py
@@ -1,7 +1,8 @@
 from ninja import NinjaAPI, ModelSchema, Schema
-from .models import User
+from .models import User, Team
+from django.core.exceptions import ObjectDoesNotExist
 
-api = NinjaAPI()
+api = NinjaAPI(urls_namespace="userapi")
 
 
 class BotRegisterSchema(Schema):
@@ -11,7 +12,13 @@ class BotRegisterSchema(Schema):
 class UserSchema(ModelSchema):
     class Config:
         model = User
-        model_fields = ['id', 'first_name', 'last_name', 'telegram']
+        model_fields = ['id', 'first_name', 'last_name', 'telegram', 'qr', 'team']
+
+
+class TeamSchema(ModelSchema):
+    class Config:
+        model = Team
+        model_fields = ['id', 'name', 'balance', 'tutor']
 
 
 @api.post("/register", response=UserSchema)
@@ -20,3 +27,10 @@ def register(request, data: BotRegisterSchema):
     user = User.objects.get(telegram=telegram)
 
     return user
+
+
+@api.get("/team", response=TeamSchema)
+def get_team(request, user_id: int):
+    user = User.objects.get(pk=user_id)
+    team = user.team
+    return team
